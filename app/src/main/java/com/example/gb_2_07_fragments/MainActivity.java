ef9f08a052ac8abcb850b398e82c7e17d0014115
gravitana <v.a.grahov@ya.ru>
2021-05-14 10:59:27 +0500

    Менеджер фрагментов, Настройки

--- app/src/main/java/com/example/gb_2_07_fragments/MainActivity.java
+++ app/src/main/java/com/example/gb_2_07_fragments/MainActivity.java
@@ -1,8 +1,16 @@
 package com.example.gb_2_07_fragments;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,116 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        readSettings();
+        initView();
+    }
+
+    private void readSettings() {
+        // Специальный класс для хранения настроек
+        SharedPreferences sharedPref = getSharedPreferences(Settings.SHARED_PREFERENCE_NAME, MODE_PRIVATE);
+
+        // Считываем значения настроек
+        Settings.isBackStack = sharedPref.getBoolean(Settings.IS_BACK_STACK_USED, false);
+        Settings.isAddFragment = sharedPref.getBoolean(Settings.IS_ADD_FRAGMENT_USED, true);
+        Settings.isBackAsRemove = sharedPref.getBoolean(Settings.IS_BACK_AS_REMOVE_FRAGMENT, true);
+        Settings.isDeleteBeforeAdd = sharedPref.getBoolean(Settings.IS_DELETE_FRAGMENT_BEFORE_ADD, false);
+    }
+
+    private void initView() {
+        initButtonMain();
+        initButtonFavorite();
+        initButtonSettings();
+        initButtonBack();
+    }
+
+    private void initButtonMain() {
+        Button buttonMain = findViewById(R.id.buttonMain);
+        buttonMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addFragment(new MainFragment());
+            }
+        });
+    }
+
+    private void initButtonFavorite() {
+        Button buttonFavorite = findViewById(R.id.buttonFavorite);
+        buttonFavorite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addFragment(new FavoriteFragment());
+            }
+        });
+    }
+
+    private void initButtonSettings() {
+        Button buttonSettings = findViewById(R.id.buttonSettings);
+        buttonSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addFragment(new SettignsFragment());
+            }
+        });
+    }
+
+    private void initButtonBack() {
+        Button buttonBack = findViewById(R.id.buttonBack);
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                if (Settings.isBackAsRemove){
+                    Fragment fragment = getVisibleFragment(fragmentManager);
+                    if (fragment != null) {
+                        fragmentManager.beginTransaction().remove(fragment).commit();
+                    }
+                } else {
+                    fragmentManager.popBackStack();
+                }
+            }
+        });
+    }
+
+    private void addFragment(Fragment fragment) {
+
+        //Получить менеджер фрагментов
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        // Открыть транзакцию
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        // Удалить видимый фрагмент
+        if (Settings.isDeleteBeforeAdd){
+            Fragment fragmentToRemove = getVisibleFragment(fragmentManager);
+            if (fragmentToRemove != null) {
+                fragmentTransaction.remove(fragmentToRemove);
+            }
+        }
+
+        // Добавить фрагмент
+        if (Settings.isAddFragment) {
+            fragmentTransaction.add(R.id.fragment_container, fragment);
+        } else {
+            fragmentTransaction.replace(R.id.fragment_container, fragment);
+        }
+
+        // Добавить транзакцию в бэкстек
+        if (Settings.isBackStack){
+            fragmentTransaction.addToBackStack(null);
+        }
+
+        // Закрыть транзакцию
+        fragmentTransaction.commit();
+    }
+
+    private Fragment getVisibleFragment(FragmentManager fragmentManager) {
+        List<Fragment> fragments = fragmentManager.getFragments();
+        for (Fragment fragment : fragments) {
+            if (fragment.isVisible())
+                return fragment;
+        }
+
+        return null;
     }
 }
---
+++ app/src/main/java/com/example/gb_2_07_fragments/MainFragment.java
@@ -0,0 +1,23 @@
+package com.example.gb_2_07_fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainFragment extends Fragment {
+
+    public MainFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        return view;
+    }
+}
