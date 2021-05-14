b5281bcdc2e0254746c13f0d471c278242ab8e70
gravitana <v.a.grahov@ya.ru>
2021-05-14 11:41:00 +0500

    меню фрагмента

--- app/src/main/java/com/example/gb_2_07_fragments/MainFragment.java
+++ app/src/main/java/com/example/gb_2_07_fragments/MainFragment.java
@@ -2,11 +2,16 @@ package com.example.gb_2_07_fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 public class MainFragment extends Fragment {
 
@@ -18,6 +23,21 @@ public class MainFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
+        setHasOptionsMenu(true); // пункты из фрагмента добавятся в главное меню
         return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.main_fragment, menu);
+        //super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.action_add) {
+            Toast.makeText(getContext(), "Chosen add", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
